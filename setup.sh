d02a7eaaa582ee1cd175fb38f6caf46344d296dc
Arnav Das <arnav.das88@gmail.com>
2025-04-28 10:39:36 +0000

    Update + nui added
    Signed-off-by: Arnav Das <arnav.das88@gmail.com>

--- setup.sh
+++ setup.sh
@@ -6,6 +6,14 @@ nsc edit operator -H ./nats-config/ --keystore-dir ./nats-config/ -u http://loca
 nsc edit operator -H ./nats-config/ --keystore-dir ./nats-config/ --account-jwt-server-url nats://localhost:4222
 nsc generate config -H ./nats-config/ --keystore-dir ./nats-config/ --nats-resolver --config-file ./nats-server/resolver.conf
 
+nsc add user -H ./nats-config/ --keystore-dir ./nats-config/ -a iifon  -n ACC-1 --allow-pub pub.acc1 --allow-sub pub.acc1
+
+chmod 777 ./nats-config/creds/iifon/SYS/sys.creds
+chmod 777 ./nats-config/creds/iifon/iifon/ACC-1.creds
+
 # start nats-server with server.conf
 
 nsc push -H ./nats-config/ --keystore-dir ./nats-config/ --all
+
+
+
